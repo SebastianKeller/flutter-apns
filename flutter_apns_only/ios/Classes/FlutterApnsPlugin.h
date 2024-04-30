472a4f7440d33551908d92512d9cd95ff2ff8391
Roman Bugaian <roman.bugaian@pwc.com>
2024-04-30 12:14:44 +0200

    Attempt to fix failing iOS build.

---
+++ flutter_apns_only/ios/Classes/FlutterApnsPlugin.h
@@ -0,0 +1,12 @@
+//
+//  FlutterApnsPlugin.h
+//  flutter_apns_only
+//
+//  Created by Roman Bugaian on 29.04.24.
+//
+
+#import <Flutter/Flutter.h>
+
+@interface FlutterApnsPlugin : NSObject<FlutterPlugin>
+
+@end
